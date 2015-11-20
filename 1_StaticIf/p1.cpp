9199c78e994f0aa3edb2a85bf16ca80e678c5561
Vittorio Romeo <vittorio.romeo@outlook.com>
2015-11-20 11:36:10 +0100

    First part template

---
+++ 1_StaticIf/p1.cpp
@@ -0,0 +1,8 @@
+// Copyright (c) 2015 Vittorio Romeo
+// License: AFL 3.0 | https://opensource.org/licenses/AFL-3.0
+// http://vittorioromeo.info | vittorio.romeo@outlook.com
+
+int main()
+{
+    return 0;
+}
